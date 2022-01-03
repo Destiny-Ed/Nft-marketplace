03287bd34e5e6cd1f68588ee3913a382d67453ae
Destiny-Ed <dikeachaeze@gmail.com>
2022-01-03 15:11:44 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: RouteGenerator.startPage,
-      onGenerateRoute:RouteGenerator.generateRoute ,
+      onGenerateRoute:RouteGenerator.generateRoute,
     );
   }
 }
+
